5744c1205355c718383bf9021500ddcce087a322
Anubhav Chaudhary <anubhav16013@iiitd.ac.in>
2019-03-14 02:36:55 +0530

    Optimized code to generate only boundary colliders

--- Assets/Scripts/VoxelGenerator.cs
+++ Assets/Scripts/VoxelGenerator.cs
@@ -6,31 +6,31 @@ public class VoxelGenerator : MonoBehaviour
 {
     // This first list contains every vertex of the mesh that we are going to render
     public List<Vector3> newVertices = new List<Vector3>();
-    
+
     // The triangles tell Unity how to build each section of the mesh joining
     // the vertices
     public List<int> newTriangles = new List<int>();
-    
+
     // The UV list is unimportant right now but it tells Unity how the texture is
     // aligned on each polygon
     public List<Vector2> newUV = new List<Vector2>();
-    
+
     // To hold the type of block
     public byte[,] blocks;
-    
+
     // Handle collider
     public List<Vector3> colVertices = new List<Vector3>();
     public List<int> colTriangles = new List<int>();
-    
-    
+
+
     // A mesh is made up of the vertices, triangles and UVs we are going to define,
     // after we make them up we'll save them as this mesh
     private Mesh mesh;
     private float tUnit = 0.25f;
-    private Vector2 tStone = new Vector2 (1, 0);
-    private Vector2 tGrass = new Vector2 (0, 1);
+    private Vector2 tStone = new Vector2(1, 0);
+    private Vector2 tGrass = new Vector2(0, 1);
     private int squareCount; // To count which square are we rendering
-    
+
     // Handle collider
     private int colCount;
     private MeshCollider col;
@@ -38,9 +38,9 @@ public class VoxelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mesh = GetComponent<MeshFilter> ().mesh;
+        mesh = GetComponent<MeshFilter>().mesh;
         col = GetComponent<MeshCollider>();
-        
+
         GenTerrain();
         BuildMesh();
         UpdateMesh();
@@ -73,122 +73,151 @@ public class VoxelGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
+
     // User defined functions below this point
 
     void UpdateMesh()
     {
 //        if (mesh == null) return;
-        mesh.Clear ();
+        mesh.Clear();
         mesh.vertices = newVertices.ToArray();
         mesh.triangles = newTriangles.ToArray();
         mesh.uv = newUV.ToArray();
 //        mesh.Optimize ();
-        mesh.RecalculateNormals ();
-        
+        mesh.RecalculateNormals();
+
         // Handle mesgCoolider
         Mesh newMesh = new Mesh();
         newMesh.vertices = colVertices.ToArray();
         newMesh.triangles = colTriangles.ToArray();
-        col.sharedMesh= newMesh;
- 
+        col.sharedMesh = newMesh;
+
         colVertices.Clear();
         colTriangles.Clear();
-        colCount=0;
+        colCount = 0;
 
         squareCount = 0;
         newVertices.Clear();
         newTriangles.Clear();
         newUV.Clear();
     }
-    
-    void GenerateCollider(int x, int y){
+
+    void GenerateCollider(int x, int y)
+    {
+//        Debug.Log(Block(x, y + 1));
+//        Debug.Log(Block(x, y - 1));
+//        Debug.Log(Block(x + 1, y));
+//        Debug.Log(Block(x - 1, y));
         //Top
-        colVertices.Add( new Vector3 (x  , y  , 1));
-        colVertices.Add( new Vector3 (x + 1 , y  , 1));
-        colVertices.Add( new Vector3 (x + 1 , y  , 0 ));
-        colVertices.Add( new Vector3 (x  , y  , 0 ));
-   
-        ColliderTriangles();
-   
-        ++colCount;
-   
+        if (Block(x, y + 1) == 0)
+        {
+            colVertices.Add(new Vector3(x, y, 1));
+            colVertices.Add(new Vector3(x + 1, y, 1));
+            colVertices.Add(new Vector3(x + 1, y, 0));
+            colVertices.Add(new Vector3(x, y, 0));
+
+            ColliderTriangles();
+            ++colCount;
+        }
+
         //bot
-        colVertices.Add( new Vector3 (x  , y -1 , 0));
-        colVertices.Add( new Vector3 (x + 1 , y -1 , 0));
-        colVertices.Add( new Vector3 (x + 1 , y -1 , 1 ));
-        colVertices.Add( new Vector3 (x  , y -1 , 1 ));
-   
-        ColliderTriangles();
-        ++colCount;
-   
+        if (Block(x, y - 1) == 0)
+        {
+            colVertices.Add(new Vector3(x, y - 1, 0));
+            colVertices.Add(new Vector3(x + 1, y - 1, 0));
+            colVertices.Add(new Vector3(x + 1, y - 1, 1));
+            colVertices.Add(new Vector3(x, y - 1, 1));
+
+            ColliderTriangles();
+            ++colCount;
+        }
+
         //left
-        colVertices.Add( new Vector3 (x  , y -1 , 1));
-        colVertices.Add( new Vector3 (x  , y  , 1));
-        colVertices.Add( new Vector3 (x  , y  , 0 ));
-        colVertices.Add( new Vector3 (x  , y -1 , 0 ));
-   
-        ColliderTriangles();
-   
-        ++colCount;
-   
+        if (Block(x - 1, y) == 0)
+        {
+            colVertices.Add(new Vector3(x, y - 1, 1));
+            colVertices.Add(new Vector3(x, y, 1));
+            colVertices.Add(new Vector3(x, y, 0));
+            colVertices.Add(new Vector3(x, y - 1, 0));
+
+            ColliderTriangles();
+            ++colCount;
+        }
+
         //right
-        colVertices.Add( new Vector3 (x +1 , y  , 1));
-        colVertices.Add( new Vector3 (x +1 , y -1 , 1));
-        colVertices.Add( new Vector3 (x +1 , y -1 , 0 ));
-        colVertices.Add( new Vector3 (x +1 , y  , 0 ));
-   
-        ColliderTriangles();
-   
-        ++colCount;
+        if (Block(x + 1, y) == 0)
+        {
+            colVertices.Add(new Vector3(x + 1, y, 1));
+            colVertices.Add(new Vector3(x + 1, y - 1, 1));
+            colVertices.Add(new Vector3(x + 1, y - 1, 0));
+            colVertices.Add(new Vector3(x + 1, y, 0));
+
+            ColliderTriangles();
+            ++colCount;
+        }
     }
-    
-    void GenTerrain(){
-        blocks=new byte[10,10];
-  
-        for(int px=0; px < blocks.GetLength(0); ++px){
-            for(int py=0; py < blocks.GetLength(1); ++py){
-                if (py == 5) 
+
+    void GenTerrain()
+    {
+        blocks = new byte[10, 10];
+
+        for (int px = 0; px < blocks.GetLength(0); ++px)
+        {
+            for (int py = 0; py < blocks.GetLength(1); ++py)
+            {
+                if (py == 5)
                 {
-                    blocks[px,py]=2;
+                    blocks[px, py] = 2;
                 }
-                else if (py < 5) 
+                else if (py < 5)
                 {
-                    blocks[px,py]=1;
+                    blocks[px, py] = 1;
                 }
             }
         }
     }
-    
-    void BuildMesh(){
-        for(int px=0; px < blocks.GetLength(0); ++px){
-            for(int py=0; py < blocks.GetLength(1); ++py){
+
+    void BuildMesh()
+    {
+        for (int px = 0; px < blocks.GetLength(0); ++px)
+        {
+            for (int py = 0; py < blocks.GetLength(1); ++py)
+            {
                 if (blocks[px, py] != 0)
                 {
                     GenerateCollider(px, py);
                 }
-                
-                if(blocks[px,py] == 1) 
+
+                if (blocks[px, py] == 1)
                 {
-                    GenerateSquare(px,py,tStone);
-                } 
-                else if(blocks[px,py] == 2) 
-                {
-                    GenerateSquare(px,py,tGrass);
+                    GenerateSquare(px, py, tStone);
                 }
-    
+                else if (blocks[px, py] == 2)
+                {
+                    GenerateSquare(px, py, tGrass);
+                }
             }
         }
     }
-    
-    void ColliderTriangles(){
+
+    void ColliderTriangles()
+    {
         colTriangles.Add(colCount * 4);
         colTriangles.Add((colCount * 4) + 1);
         colTriangles.Add((colCount * 4) + 3);
         colTriangles.Add((colCount * 4) + 1);
         colTriangles.Add((colCount * 4) + 2);
         colTriangles.Add((colCount * 4) + 3);
+    }
+
+    byte Block(int x, int y)
+    {
+        if (x == -1 || x == blocks.GetLength(0) || y == -1 || y == blocks.GetLength(1))
+        {
+            return (byte) 0;
+        }
+
+        return blocks[x, y];
     }
 }
