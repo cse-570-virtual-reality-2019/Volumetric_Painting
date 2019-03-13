48e0a1edb0786559d063f8feab1bf771193dfb64
Anubhav Chaudhary <anubhav16013@iiitd.ac.in>
2019-03-14 01:57:33 +0530

    2d voxel loading in grid with different texture

--- Assets/Scripts/VoxelGenerator.cs
+++ Assets/Scripts/VoxelGenerator.cs
@@ -15,52 +15,100 @@ public class VoxelGenerator : MonoBehaviour
     // aligned on each polygon
     public List<Vector2> newUV = new List<Vector2>();
     
+    // To hold the type of block
+    public byte[,] blocks;
+    
     
     // A mesh is made up of the vertices, triangles and UVs we are going to define,
     // after we make them up we'll save them as this mesh
     private Mesh mesh;
     private float tUnit = 0.25f;
-    private Vector2 tStone = new Vector2 (0, 0);
+    private Vector2 tStone = new Vector2 (1, 0);
     private Vector2 tGrass = new Vector2 (0, 1);
+    private int squareCount; // To count which square are we rendering
 
     // Start is called before the first frame update
     void Start()
     {
         mesh = GetComponent<MeshFilter> ().mesh;
-   
-        float x = transform.position.x;
-        float y = transform.position.y;
-        float z = transform.position.z;
-   
-   
-        newVertices.Add( new Vector3 (x  , y  , z ));
-        newVertices.Add( new Vector3 (x + 1 , y  , z ));
-        newVertices.Add( new Vector3 (x + 1 , y-1  , z ));
-        newVertices.Add( new Vector3 (x  , y-1  , z ));
-   
-        newTriangles.Add(0);
-        newTriangles.Add(1);
-        newTriangles.Add(3);
-        newTriangles.Add(1);
-        newTriangles.Add(2);
-        newTriangles.Add(3);
-        
-        newUV.Add(new Vector2 (tUnit * tStone.x, tUnit * tStone.y + tUnit));
-        newUV.Add(new Vector2 (tUnit * tStone.x + tUnit, tUnit * tStone.y + tUnit));
-        newUV.Add(new Vector2 (tUnit * tStone.x + tUnit, tUnit * tStone.y));
-        newUV.Add(new Vector2 (tUnit * tStone.x, tUnit * tStone.y));
-   
-        
+        GenTerrain();
+        BuildMesh();
+        UpdateMesh();
+//        GenerateSquare(x, y);
+    }
+
+    private void GenerateSquare(int x, int y, Vector2 texture)
+    {
+        float z;
+        newVertices.Add(new Vector3(x, y, 0));
+        newVertices.Add(new Vector3(x + 1, y, 0));
+        newVertices.Add(new Vector3(x + 1, y - 1, 0));
+        newVertices.Add(new Vector3(x, y - 1, 0));
+
+        newTriangles.Add(squareCount * 4);
+        newTriangles.Add((squareCount * 4) + 1);
+        newTriangles.Add((squareCount * 4) + 3);
+        newTriangles.Add((squareCount * 4) + 1);
+        newTriangles.Add((squareCount * 4) + 2);
+        newTriangles.Add((squareCount * 4) + 3);
+
+        newUV.Add(new Vector2(tUnit * texture.x, tUnit * texture.y + tUnit));
+        newUV.Add(new Vector2(tUnit * texture.x + tUnit, tUnit * texture.y + tUnit));
+        newUV.Add(new Vector2(tUnit * texture.x + tUnit, tUnit * texture.y));
+        newUV.Add(new Vector2(tUnit * texture.x, tUnit * texture.y));
+
+        ++squareCount;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    
+    // User defined functions below this point
+
+    void UpdateMesh()
+    {
+//        if (mesh == null) return;
         mesh.Clear ();
         mesh.vertices = newVertices.ToArray();
         mesh.triangles = newTriangles.ToArray();
         mesh.uv = newUV.ToArray();
 //        mesh.Optimize ();
         mesh.RecalculateNormals ();
+
+        squareCount = 0;
+        newVertices.Clear();
+        newTriangles.Clear();
+        newUV.Clear();
+    }
+    
+    void GenTerrain(){
+        blocks=new byte[10,10];
+  
+        for(int px=0;px<blocks.GetLength(0);px++){
+            for(int py=0;py<blocks.GetLength(1);py++){
+                if(py==5){
+                    blocks[px,py]=2;
+                } else if(py<5){
+                    blocks[px,py]=1;
+                }
+            }
+        }
+    }
+    
+    void BuildMesh(){
+        for(int px=0;px<blocks.GetLength(0);px++){
+            for(int py=0;py<blocks.GetLength(1);py++){
+    
+                if(blocks[px,py]==1){
+                    GenerateSquare(px,py,tStone);
+                } else if(blocks[px,py]==2){
+                    GenerateSquare(px,py,tGrass);
+                }
+    
+            }
+        }
     }
 }
