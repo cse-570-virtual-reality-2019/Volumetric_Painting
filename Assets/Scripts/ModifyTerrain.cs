86deb72253f33ba25559252fa3fae2dcda6a2172
Anubhav Chaudhary <anubhav16013@iiitd.ac.in>
2019-03-31 18:03:23 +0530

    Added movement to camera position so that user can rotate after pressing space and use wasd accordingly

--- Assets/Scripts/ModifyTerrain.cs
+++ Assets/Scripts/ModifyTerrain.cs
@@ -11,10 +11,20 @@ public class ModifyTerrain : MonoBehaviour
     public GameObject vive_controller_emulator;
     public bool useViveEmulator = false;
 
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    public float moveSpeed = 1.0f;
+    public float sprintSpeed = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private Transform cameraTransform;
+
     void Start()
     {
         world = gameObject.GetComponent("World") as World;
         cameraGO = GameObject.FindGameObjectWithTag("MainCamera");
+        cameraTransform = Camera.main.transform;
     }
 
     private void Update()
@@ -28,52 +38,70 @@ public class ModifyTerrain : MonoBehaviour
         {
             AddBlockCursor(1);
         }
+
+        if (Input.GetKey("space"))
+    	{
+	        yaw += speedH * Input.GetAxis("Mouse X");
+	        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+	        cameraTransform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+	        // if (Input.GetKey("w"))
+	        // 	Camera.main.transform.position = Camera.main.transform.forward * moveForwardSpeed * Time.deltaTime;
+
+	        if (Input.GetKey("w")) {
+	            if (Input.GetKey (KeyCode.LeftShift)) {
+					cameraTransform.position += cameraTransform.forward * (Time.deltaTime * sprintSpeed);
+					// Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 80, 2*Time.deltaTime);
+	            }
+	            else {
+	                cameraTransform.position += cameraTransform.forward * (Time.deltaTime * moveSpeed);
+	                // Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60, 2*Time.deltaTime);
+	            }
+	        }
+	        if (Input.GetKey("s")) {
+	            cameraTransform.position += cameraTransform.forward * (Time.deltaTime * (-moveSpeed));
+	        }
+	    }
     }
 
     public void ReplaceBlockCenter(float range, byte block)
     {
         //Replaces the block directly in front of the player
-        Ray ray = new Ray(cameraGO.transform.position, cameraGO.transform.forward);
+        var ray = new Ray(cameraGO.transform.position, cameraGO.transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
+        if (!Physics.Raycast(ray, out hit)) return;
+        if (hit.distance < range)
         {
-            if (hit.distance < range)
-            {
-                ReplaceBlockAt(hit, block);
-            }
+            ReplaceBlockAt(hit, block);
         }
     }
 
     public void AddBlockCenter(float range, byte block)
     {
         //Adds the block specified directly in front of the player
-        Ray ray = new Ray(cameraGO.transform.position, cameraGO.transform.forward);
+        var ray = new Ray(cameraGO.transform.position, cameraGO.transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
+        if (!Physics.Raycast(ray, out hit)) return;
+        if (hit.distance < range)
         {
-            if (hit.distance < range)
-            {
-                AddBlockAt(hit, block);
-            }
-
-            Debug.DrawLine(ray.origin, ray.origin + (ray.direction * hit.distance), Color.green, 2);
+            AddBlockAt(hit, block);
         }
+
+        Debug.DrawLine(ray.origin, ray.origin + (ray.direction * hit.distance), Color.green, 2);
     }
 
     public void ReplaceBlockCursor(byte block)
     {
         //Replaces the block specified where the mouse cursor is pointing
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            ReplaceBlockAt(hit, block);
-            Debug.DrawLine(ray.origin, ray.origin + (ray.direction * hit.distance),
-                Color.green, 2);
-        }
+        if (!Physics.Raycast(ray, out hit)) return;
+        ReplaceBlockAt(hit, block);
+        Debug.DrawLine(ray.origin, ray.origin + (ray.direction * hit.distance),
+            Color.green, 2);
     }
 
     public void AddBlockCursor(byte block)
@@ -86,7 +114,7 @@ public class ModifyTerrain : MonoBehaviour
         }
         else
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit))
@@ -107,7 +135,7 @@ public class ModifyTerrain : MonoBehaviour
     public void ReplaceBlockAt(RaycastHit hit, byte block)
     {
         //removes a block at these impact coordinates, you can raycast against the terrain and call this with the hit.point
-        Vector3 position = hit.point;
+        var position = hit.point;
         position += (hit.normal * -0.5f);
 
         SetBlockAt(position, block);
@@ -116,7 +144,7 @@ public class ModifyTerrain : MonoBehaviour
     public void AddBlockAt(RaycastHit hit, byte block)
     {
         //adds the specified block at these impact coordinates, you can raycast against the terrain and call this with the hit.point
-        Vector3 position = hit.point;
+        var position = hit.point;
         position += (hit.normal * 0.5f);
 
         SetBlockAt(position, block);
@@ -124,7 +152,7 @@ public class ModifyTerrain : MonoBehaviour
 
     public void AddBlockAt(Vector3 point, byte block)
     {
-        Vector3 position = point;
+        var position = point;
         position += Vector3.up * 0.5f;
 
         SetBlockAt(position, block);
@@ -133,9 +161,9 @@ public class ModifyTerrain : MonoBehaviour
     public void SetBlockAt(Vector3 position, byte block)
     {
         //sets the specified block at these coordinates
-        int x = Mathf.RoundToInt(position.x);
-        int y = Mathf.RoundToInt(position.y);
-        int z = Mathf.RoundToInt(position.z);
+        var x = Mathf.RoundToInt(position.x);
+        var y = Mathf.RoundToInt(position.y);
+        var z = Mathf.RoundToInt(position.z);
 
         SetBlockAt(x, y, z, block);
     }
@@ -153,9 +181,9 @@ public class ModifyTerrain : MonoBehaviour
     public void UpdateChunkAt(int x, int y, int z)
     {
         //Updates the chunk containing this block
-        int updateX = Mathf.FloorToInt(x / world.chunkSize);
-        int updateY = Mathf.FloorToInt(y / world.chunkSize);
-        int updateZ = Mathf.FloorToInt(z / world.chunkSize);
+        var updateX = Mathf.FloorToInt(x / world.chunkSize);
+        var updateY = Mathf.FloorToInt(y / world.chunkSize);
+        var updateZ = Mathf.FloorToInt(z / world.chunkSize);
 
         print("Updating: " + updateX + ", " + updateY + ", " + updateZ);
 
