c4cb6618cfe15973d51c7f4ffd09c40280f6a573
Anubhav Chaudhary <anubhav16013@iiitd.ac.in>
2019-04-13 23:13:45 +0530

    Followed tutorial 2 for creating menu
    Edited input_system in player to add script, disabled inbuild script.
    Need to add a button as an action to actually click, and follow tutorial
    2 ending to check if it works

--- Assets/Scripts/VRInputModule.cs
+++ Assets/Scripts/VRInputModule.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
 
-public class VRInputModule : MonoBehaviour
+public class VRInputModule : BaseInputModule
 {
+    public Camera m_Camera;
+    public SteamVR_Input_Sources m_TargetSource;
+    public SteamVR_Action_Boolean m_ClickAction;
+
+    private GameObject m_CurrentObject = null;
+    private PointerEventData m_Data = null;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +22,58 @@ public class VRInputModule : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        base.Awake();
+        
+        m_Data = new PointerEventData(eventSystem);
+    }
+
+    public override void Process()
+    {
+        // Reset Data, set Camera
+        m_Data.Reset();
+        m_Data.position = new Vector2(m_Camera.pixelWidth / 2, m_Camera.pixelHeight /2);
+        
+        // Raycast
+        eventSystem.RaycastAll(m_Data, m_RaycastResultCache);
+        m_Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        m_CurrentObject = m_Data.pointerCurrentRaycast.gameObject;
+
+        // Clear
+        m_RaycastResultCache.Clear();
+
+        // Hover
+        HandlePointerExitAndEnter(m_Data, m_CurrentObject);
+
+        // Press
+        if (m_ClickAction.GetStateDown(m_TargetSource))
+        {
+            ProcessPress(m_Data);
+        }
+
+        // Release
+        if (m_ClickAction.GetStateDown(m_TargetSource))
+        {
+            ProcessRelease(m_Data);
+        }
+    }
+
+    public PointerEventData GetData()
+    {
+        return m_Data;
+    }
+
+    private void ProcessPress(PointerEventData data)
+    {
+        
+    }
+
+    private void ProcessRelease(PointerEventData data)
     {
         
     }
