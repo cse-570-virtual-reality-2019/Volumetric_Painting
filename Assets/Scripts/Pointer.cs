49c01bdf97c4ba91e1f2319dca498e887d684598
Anubhav Chaudhary <anubhav16013@iiitd.ac.in>
2019-04-13 19:08:26 +0530

    Line renderer added, right controller should display a line pointing outwards to show where the controller is looking

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
+    public float m_DefaultLength = 5.0f;
+    public GameObject m_Dot;
+    public VRInputModule m_InputModule;
+
+    private LineRenderer m_LineRenderer = null;
+    
     private void Awake()
     {
-        throw new System.NotImplementedException();
+        m_LineRenderer = GetComponent<LineRenderer>();
     }
 
     // Start is called before the first frame update
@@ -18,17 +24,42 @@ public class Pointer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateLine();   
     }
 
     private void UpdateLine()
     {
-        
+        // Use default or distance
+        float targetLength = m_DefaultLength;
+
+        // Raycast
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        // Default 
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+        // Or based on hit
+        if (hit.collider != null)
+        {
+            endPosition = hit.point;
+        }
+
+        // Set position of the dot
+        m_Dot.transform.position = endPosition;
+
+
+        // Set line renderer
+        m_LineRenderer.SetPosition(0, transform.position);
+        m_LineRenderer.SetPosition(1, endPosition);
     }
     
-    // Using link https://www.youtube.com/watch?v=3mRI1hu9Y3w
-    /*private RaycastHit CreateRaycast(float length)
+//    Using link https://www.youtube.com/watch?v=3mRI1hu9Y3w
+    private RaycastHit CreateRaycast(float length)
     {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, m_DefaultLength);
         
-    }*/
+        return hit;
+    }
 }
